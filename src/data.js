eb6e357ec397908822a014b5852ec5768bccca18
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2023-01-20 23:37:01 +0200

    Add Static date for testing items

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const slidItems = [
+    {
+        id: 1,
+        img: 'https://eg.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/33/810723/1.jpg?4742',
+        title: 'Winter',
+        discription: 'Dont loss the offer',
+        bg: 'f5fafd',
+    },
+    {
+        id: 2,
+        img: 'https://eg.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/33/810723/1.jpg?4742',
+        title: 'Summer',
+        discription: 'Dont loss the offer',
+        bg: 'f0fafa',
+    },
+    {
+        id: 3,
+        img: 'https://eg.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/33/810723/1.jpg?4742',
+        title: 'lol',
+        discription: 'Dont loss the offer',
+        bg: 'f5f7fd',
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/0ff4149f-9815-4f4a-a188-fcbb246376e7/sportswear-older-t-shirt-ptHM7k.png',
+        title: 'kids Nike Sportswear',
+    },
+    {
+        id: 2,
+        img: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/64477061-9fba-4456-be5d-18553c4da75c/sportswear-club-fleece-older-graphic-pullover-hoodie-X1lRV2.png',
+        title: 'Nike Sportswear Club Fleece',
+    },
+    {
+        id: 3,
+        img: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/612c05f3-2d69-4a9c-9a6e-80c931d0e5e0/billie-eilish-t-shirt-wp1zcC.png',
+        title: 'Nike Sportswear',
+    },
+]
+export const products = [
+    {
+        id: 1,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 2,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 3,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 4,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 5,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 6,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 7,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+    {
+        id: 8,
+        img: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
+    },
+]
