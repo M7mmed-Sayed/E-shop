6cf36b727bd66129d60af7c53ad29c25a6206f81
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2023-01-20 23:36:34 +0200

    Add Home components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,24 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
-
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
+import styled from 'styled-components'
+const Container = styled.div`
+`
 const Home = () => {
     return (
-        <div>
+        <Container>
             <Announcement />
             <Navbar />
-            <Slider/>
-        </div>
+            <Slider />
+            <Categories />
+            <Products />
+            <NewsLetter />
+            <Footer />
+        </Container>
     )
 }
 
