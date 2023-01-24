7e9d5597ed6447e491d5987629b5671a97b8c20b
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2023-01-24 22:52:55 +0200

    Edit Responsive file to be used with Tablet mode

--- src/responsive.js
+++ src/responsive.js
@@ -2,14 +2,14 @@ import {css} from 'styled-components'
 
 export const mobile=(props)=>{
     return css`
-        @media only screen and (max-width: 380px) {
+        @media only screen and (max-width: 420px) {
             ${props}
         }
     `
 }
 export const tablet = (props) => {
     return css`
-        @media only screen and (max-width: 380px) {
+        @media only screen and (max-width: 800px) {
             ${props}
         }
     `
